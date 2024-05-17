1a5b95bf8ad14b42bdadafa675b9211bb146b503
antoha-x <272acy@gmail.com>
2024-05-18 02:20:19 +0500

    1. complete test test_fill_form_and_check_result_table 2. add img file m527697.jpg for upload file in test 3 change selene default timeout to 8 sec 4 add to readme js script for freeze page

--- conftest.py
+++ conftest.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 def config_browser():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+    browser.config.timeout = 8
     browser.config.base_url = "https://demoqa.com/automation-practice-form"
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
--- home_work_test.py
+++ home_work_test.py
@@ -1,5 +1,40 @@
-from selene import browser, be, have
+import os
+
+from selene import browser, have
 
 
 def test_fill_form_and_check_result_table():
     browser.open('')
+    browser.element('#firstName').send_keys("Vasiliy")
+    browser.element('#lastName').send_keys("Pupkin")
+    browser.element('#userEmail').send_keys("test@test.ts")
+    browser.element('#gender-radio-1 + label').click()
+    browser.element('#userNumber').send_keys("9876543210")
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').click().element("[value='0']").click()
+    browser.element('.react-datepicker__year-select').click().element("[value='1990']").click()
+    browser.element('.react-datepicker__day--001:not(.react-datepicker__day--outside-month)').click()
+    browser.element('#subjectsInput').send_keys("Maths")
+    browser.element('#react-select-2-option-0').click()
+    browser.element("[for='hobbies-checkbox-2']").click()
+    browser.element('#uploadPicture').send_keys(f"{os.getcwd()}/resources/m527697.jpg")
+    browser.element('#currentAddress').send_keys("Royal Road, Mont Choisy Mauritius, Mont Choisy, Mauritius Island")
+    browser.element("#submit").hover()
+    browser.element("#state").click()
+    browser.element("#react-select-3-option-0").click()
+    browser.element("#city").click()
+    browser.element("#react-select-4-option-2").click()
+    browser.element("#submit").click()
+
+    browser.all(".modal-body tbody > tr > td:last-child").element(0).should(have.text("Vasiliy Pupkin"))
+    browser.all(".modal-body tbody > tr > td:last-child").element(1).should(have.text("test@test.ts"))
+    browser.all(".modal-body tbody > tr > td:last-child").element(2).should(have.text("Male"))
+    browser.all(".modal-body tbody > tr > td:last-child").element(3).should(have.text("9876543210"))
+    browser.all(".modal-body tbody > tr > td:last-child").element(4).should(have.text("01 January,1990"))
+    browser.all(".modal-body tbody > tr > td:last-child").element(5).should(have.text("Maths"))
+    browser.all(".modal-body tbody > tr > td:last-child").element(6).should(have.text("Reading"))
+    browser.all(".modal-body tbody > tr > td:last-child").element(7).should(have.text("m527697.jpg"))
+    browser.all(".modal-body tbody > tr > td:last-child").element(8).should(have.text("Royal Road, Mont Choisy "
+                                                                                      "Mauritius, Mont Choisy, "
+                                                                                      "Mauritius Island"))
+    browser.all(".modal-body tbody > tr > td:last-child").element(9).should(have.text("NCR Noida"))
